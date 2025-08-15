aed48eadffbf87ce9ed536e7b09a2b5469c421df
theseeduse <qwert304411@naver.com>
2025-08-15 20:14:54 +0900

    Hydration text content mismatch 오류 해결

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,24 @@
 <template>
-  <h1>{{ wikiPage?.isDocument ? data.docName : (wikiPage?.title || '&nbsp;') }}</h1>
+  <h1>{{ hydrated ? heading : '\u00A0' }}</h1>
   <NuxtPage />
 </template>
+
+<script setup lang="ts">
+const wikiPage = useState('wikiPage')
+
+const { data } = useFetch('/i/getDocument', { lazy: true })
+
+const hydrated = ref(false)
+onMounted(() => { hydrated.value = true })
+
+const heading = computed(() => {
+  if (wikiPage?.isDocument) {
+    return data?.docName || wikiPage?.title || '\u00A0'
+  }
+  return wikiPage?.title || '\u00A0'
+})
+</script>
 
 <style>
 /* @import '@/assets/styles/main.css'; */
 </style>
-
-<script setup>
-const { data } = await useFetch('/i/getDocument')
-
-const wikiPage = useState('wikiPage')
-</script>
