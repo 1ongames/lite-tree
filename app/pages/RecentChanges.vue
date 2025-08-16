d042ca2db5f9fe91d795478fa79466d3e14099a2
theseeduse <qwert304411@naver.com>
2025-08-16 11:11:25 +0900

    asdf

--- app/pages/RecentChanges.vue
+++ app/pages/RecentChanges.vue
@@ -1,5 +1,15 @@
 <template>
     <RecentBar :type="1" optType="all" />
+
+    <div class="change-menu">
+      <div class="change-navbar change-header">
+        <div class="change-value">문서</div>
+        <div class="change-value">기능</div>
+        <div class="change-value">수정자</div>
+        <div class="change-value">수정 시간</div>
+      </div>
+    <!-- <ChangeLogs v-for="" :key="" /> -->
+    </div>
 </template>
 
 <script setup>
