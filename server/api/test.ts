0550b7b6764551b7dedeb8a655bf4d8e3656dce1
theseeduse <qwert304411@naver.com>
2025-10-22 23:09:04 +0900

    api 오류 반환 테스트

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,6 +1,13 @@
-import { defineEventHandler, H3Event } from 'h3'
+import { defineEventHandler, H3Event, getQuery } from 'h3'
+import { respondErrorByCode } from '../utils/error'
 
 export default defineEventHandler((event: H3Event) => {
+  const { error } = getQuery(event)
+  
+  if (error === 'true' || error === true) {
+    return respondErrorByCode(event, '500')
+  }
+  
   return {
     hello: 'world',
     test: true
